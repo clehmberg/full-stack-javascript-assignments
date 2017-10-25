cf69f37ff9f15f862b0587bc3cb6a627582631dd
Clint Lehmberg <clintlehmberg@gmail.com>
2017-10-25 16:29:58 -0600

    completed assignment

--- src/javascript/intro/grammer/strings/index.js
+++ src/javascript/intro/grammer/strings/index.js
@@ -1,1 +1,14 @@
-export const greeting = '';
+const greeting = 'Hello World';
+
+const greetingArray = greeting.split('')
+
+const begins = (str, char) => {
+  return str [0] === char
+
+
+}
+const beginsInsensitive = (str, char) => {
+  return str.toLowerCase() [0] === char.toLowerCase()
+}
+
+console.log(beginsInsensitive(greeting, 'H'))
